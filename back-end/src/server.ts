9170821e7a500573099451c4993e0b06d96b4da7
Rashid Ahamed <rashid123321a@gmail.com>
2023-08-31 22:07:10 +0530

    feat: add cors

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,13 +1,18 @@
 import  express, { Express } from 'express';
+import cors from 'cors';
 import { getLogger } from "./helpers/logger";
-import { port } from './config/config';
+import { fe_url, port } from './config/config';
 
 const logger = getLogger('SERVER');
 const app: Express = express();
 
+app.use(cors({
+    origin: fe_url
+  }));
+
 app.get('/',(req,res) => {
     res.send("Hello World");
-})
+});
 
 app.listen(port, () => {
     logger.info(`Server is listening at: ${port}`);
