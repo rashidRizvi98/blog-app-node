7206c6c1b82cbfd67fbe0b268a96d9b5f34b5b78
Rashid Ahamed <rashid123321a@gmail.com>
2023-08-31 23:29:14 +0530

    feat: add db connection, sequelize

--- back-end/src/config/config.ts
+++ back-end/src/config/config.ts
@@ -1,3 +1,11 @@
 export const port = process.env.PORT || 4000;
 
 export const fe_url = 'http://localhost:3000';
+
+export const dbConfig = {
+    username: "postgres",
+    password: "password",
+    port: 5432,
+    host: "localhost",
+    name: "login-system"
+  };
---
+++ back-end/src/database/database.ts
@@ -0,0 +1,41 @@
+import { Sequelize } from "sequelize-typescript";
+import { createdb } from "pgtools";
+import { getLogger } from "../helpers/logger";
+import { dbConfig } from "../config/config";
+
+const logger = getLogger('DATABASE');
+
+export const connection = new Sequelize({
+    username: dbConfig.username,
+    password: dbConfig.password,
+    host: dbConfig.host,
+    port: dbConfig.port,
+    dialect: "postgres",
+    database: dbConfig.name,
+    logging: false,
+    models: []
+})
+
+export const createDatabase = async (dbName: string) => {
+    await createdb({
+        user: dbConfig.username,
+        password: dbConfig.password,
+        host: dbConfig.host,
+        port: dbConfig.port
+    }, dbName);
+}
+
+export const initializeDatabase =async () => {
+
+    try {
+        await createDatabase(dbConfig.name);        
+    } catch (error) {
+    }
+
+    try {
+        await connection.sync({force: false,alter: true});
+        logger.info("SYNCED  DB SUCCESSFULLY");  
+    } catch (error) {
+        logger.error("FAILED TO ESTABLISH DB CONNECTION: ",error);
+    }
+}
